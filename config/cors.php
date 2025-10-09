5d90cc5183ef2560684e9c2e3d23dd02e1b63c9d
bogdan-popov <bogdan_popov17@mail.ru>
2025-10-09 20:26:39 +0300

    fix cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000', 'https://zov-hozina-client.vercel.app'],
+    'allowed_origins' => ['http://localhost:3000', 'https://zov-hozina-client.vercel.app', '*.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
